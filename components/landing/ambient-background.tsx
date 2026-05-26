1772062ed872ec60c51ec072879163b9ddc2fe81
AyaanCreates <ayaanexperimental@gmail.com>
2026-05-27 01:41:58 +0530

    Enable React Bits Grainient background

--- components/landing/ambient-background.tsx
+++ components/landing/ambient-background.tsx
@@ -8,30 +8,30 @@ export function AmbientBackground() {
       <span className="ambient-background__counterflow" />
       <Grainient
         className="ambient-background__webgl"
-        timeSpeed={2.18}
-        colorBalance={0.06}
-        warpStrength={1.05}
-        warpFrequency={4.15}
-        warpSpeed={1.08}
-        warpAmplitude={24}
-        blendAngle={24}
-        blendSoftness={0.14}
-        rotationAmount={320}
-        noiseScale={1.7}
-        grainAmount={0.018}
+        timeSpeed={2.55}
+        colorBalance={0.02}
+        warpStrength={1.32}
+        warpFrequency={4.6}
+        warpSpeed={1.28}
+        warpAmplitude={19}
+        blendAngle={18}
+        blendSoftness={0.11}
+        rotationAmount={390}
+        noiseScale={2.05}
+        grainAmount={0.024}
         grainScale={2}
         grainAnimated
-        contrast={1.2}
+        contrast={1.28}
         gamma={1}
-        saturation={1.32}
+        saturation={1.42}
         centerX={0}
         centerY={-0.01}
         zoom={0.92}
         color1="#f5a7bd"
         color2="#ffe78f"
         color3="#b8d89a"
-        frameRate={26}
-        lowPowerFrameRate={16}
+        frameRate={28}
+        lowPowerFrameRate={18}
       />
       <span className="ambient-background__readability-veil" />
     </div>
