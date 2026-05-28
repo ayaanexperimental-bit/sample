fb9819fa0500617180fe7eee6c0436555bfd40ec
AyaanCreates <ayaanexperimental@gmail.com>
2026-05-28 10:47:12 +0530

    Restore pink program heading emphasis

--- components/landing/levelup-clone.tsx
+++ components/landing/levelup-clone.tsx
@@ -490,7 +490,7 @@ function ProgramBonusesSection() {
         <div className="levelup-program-bonuses__header">
           <span className="levelup-program-bonuses__kicker">Heal Your Hormones Program</span>
           <h2 id="program-bonuses-title">
-            What You&apos;ll Receive After The Program{" "}
+            What You&apos;ll Receive After The <span>Program</span>{" "}
             <span aria-hidden="true">{"\u{1F338}"}</span>
           </h2>
           <p>
