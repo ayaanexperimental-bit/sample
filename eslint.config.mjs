2be7106d11b2264d68d1cb7807f7807969406748
AyaanCreates <ayaanexperimental@gmail.com>
2026-06-05 06:50:38 +0530

    Ignore generated Cloudflare worker lint artifact

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,13 @@ import nextTypescript from "eslint-config-next/typescript";
 
 const eslintConfig = [
   {
-    ignores: [".next/**", ".wrangler/**", ".cloudflare-pages-build/**", "out/**"]
+    ignores: [
+      ".next/**",
+      ".wrangler/**",
+      ".cloudflare-pages-build/**",
+      ".cloudflare-pages-worker/**",
+      "out/**"
+    ]
   },
   ...nextVitals,
   ...nextTypescript
